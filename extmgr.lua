a93b4ebb94d9aced7fea669770996c7187f4cd7a
Albert Krewinkel <albert@zeitkraut.de>
2024-05-05 10:47:42 +0200

    Make it so! ✨🚀

---
+++ extmgr.lua
@@ -0,0 +1,60 @@
+local json = require 'pandoc.json'
+local path = require 'pandoc.path'
+local system = require 'pandoc.system'
+local zip = require 'pandoc.zip'
+
+local name = arg[1] or error 'no extension given'
+
+local packages_json = io.open('packages.json'):read '*a'
+local packages = json.decode(packages_json, false) or
+  error 'JSON decoding the content of packages.json failed.'
+
+local pkg = packages[name] or
+  error('Extension ' .. name .. ' not found.')
+
+if pkg.type ~= 'github' then
+  error('Unknown extension type "' .. pkg.type .. '".')
+end
+
+if not pkg.repository then
+  error('Packages of type "github" must list a repository.')
+end
+
+local zip_url = 'https://github.com/' .. pkg.repository ..
+  '/archive/refs/tags/' .. pkg.version .. '.zip'
+local mt, zip_contents = pandoc.mediabag.fetch(zip_url)
+
+print(mt, 'length: ' .. tostring(#zip_contents))
+
+local archive = zip.Archive(zip_contents)
+local filter_filename = name .. '.lua'
+
+local extension = {}
+for k, entry in pairs(archive.entries) do
+  if path.filename(entry.path) == filter_filename then
+    extension.filter = extension.filter
+      and (#extension.filter > #entry:contents()
+           and extension.filter
+           or entry:contents())
+      or entry:contents()
+  end
+  if path.filename(entry.path) == 'LICENSE' then
+    extension.license = entry:contents()
+  end
+end
+
+if not extension.filter then
+  error('Found no filter in the zip.')
+end
+if not extension.license then
+  error('Found no license in the zip.')
+end
+
+local target_file = {
+  filter = 'filter/' .. filter_filename,
+  license = 'filter/' .. name .. '.license',
+}
+
+pandoc.mediabag.insert(target_file.filter, 'text/x-lua', extension.filter)
+pandoc.mediabag.insert(target_file.license, 'text/plain', extension.license)
+pandoc.mediabag.write('.')
